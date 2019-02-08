b3d94454c8dd443ab8ba1a09d8ab88763e91d831
Damien Rogé <damien.roge.info@gmail.com>
2019-02-08 08:32:46 +0100

    Create CSVParserTest.cpp
    add blank test

---
+++ csv/CSVParserTest.cpp
@@ -0,0 +1,17 @@
+/**
+ * AUTEUR : Damien ROGÉ
+ * DATE : 19 JANVIER 2019
+ **/
+
+#include "CSVParser.hpp"
+
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#include "catch.hpp"
+
+using namespace std;
+
+
+TEST_CASE( "", "[]" ) {
+
+
+}
